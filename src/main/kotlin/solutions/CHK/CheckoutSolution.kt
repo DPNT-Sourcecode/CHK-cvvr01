6cc78b9740d73a48919c7d73e97eaa34d71ee1cf
sbx_user1051 <sbx_user1051@169.254.61.5>
2024-01-26 12:43:03 +0000

    Fri Jan 26 12:43:03 UTC 2024

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -2,6 +2,35 @@ package solutions.CHK
 
 object CheckoutSolution {
     fun checkout(skus: String): Int {
-        TODO("Solution not implemented")
+        val items = setupItems()
+        var skuList = skus.split("")
+        validateSkus(skuList)
+        skuList = toUpperCase(skuList)
+
+    }
+
+    private fun toUpperCase(skuList: List<String>): List<String> {
+        return skuList.map { it.uppercase() }
+    }
+
+    private fun validateSkus(skuList: List<String>) {
+        skuList.forEach {
+            if (!it.matches(Regex("[A-Z]"))) {
+                throw IllegalArgumentException("Invalid SKU: $it")
+            }
+        }
+    }
+
+    private fun setupItems(): List<Item> {
+        return listOf(
+            Item('A', 50, listOf(SpecialOffer(3, 130))),
+            Item('B', 30, listOf(SpecialOffer(2, 45))),
+            Item('C', 20),
+            Item('D', 15)
+        )
     }
 }
+
+data class Item(val sku: Char, val price: Int, val specialOffers: List<SpecialOffer> = emptyList())
+
+data class SpecialOffer(val quantity: Int, val price: Int)
--- src/test/kotlin/solutions/CHK/CheckoutTest.kt
+++ src/test/kotlin/solutions/CHK/CheckoutTest.kt
@@ -1,4 +1,14 @@
 package solutions.CHK
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
 class CheckoutTest {
+
+    @Test
+    fun invalidSkusThrowError() {
+        Assertions.assertThrows(IllegalArgumentException::class.java) {
+            CheckoutSolution.checkout("123")
+        }
+    }
 }

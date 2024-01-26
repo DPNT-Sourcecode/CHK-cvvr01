68806e084b320d3f8c53024270b0a89ee2afa80d
sbx_user1051 <sbx_user1051@169.254.61.5>
2024-01-26 12:13:20 +0000

    Fri Jan 26 12:13:20 UTC 2024

--- src/main/kotlin/solutions/SUM/SumSolution.kt
+++ src/main/kotlin/solutions/SUM/SumSolution.kt
@@ -2,6 +2,23 @@ package solutions.SUM
 
 object SumSolution {
     fun sum(x: Int, y: Int): Int {
-        TODO("Solution not implemented")
+        validateInputNumbers(x, y)
+        return x + y
+    }
+
+    fun validateInputNumbers(x: Int, y: Int) {
+        if (x < 0) {
+            throw IllegalArgumentException("x should be positive")
+        }
+        if (x > 100) {
+            throw IllegalArgumentException("x should be max 100")
+        }
+        if (y < 0) {
+            throw IllegalArgumentException("y should be positive")
+        }
+        if (y > 100) {
+            throw IllegalArgumentException("y should be max 100")
+        }
     }
 }
+
